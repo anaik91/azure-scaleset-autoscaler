85a65ba0fb304473b46c1f45f5c8095bed5e199b
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2020-09-10 15:18:10 +0530

    handled uuid based on correlation id

--- vmss-function-app/mpscalesetwebhook01/__init__.py
+++ vmss-function-app/mpscalesetwebhook01/__init__.py
@@ -30,21 +30,28 @@ def handle_dead_uuid(vmss_data,uuid):
     vmScaleSetTags = vmScaleSetData.tags
     try:
         dead_uuid_list = vmScaleSetTags['dead_uuid_list']
-        dead_uuid_buffer = vmScaleSetTags['dead_uuid_buffer']
-        if uuid == dead_uuid_buffer:
-            return None
-        else:
-            if len(dead_uuid_list) > 0:
-                dead_uuid_list = dead_uuid_list.split(',')
-                dead_uuid_list.append(uuid)
-                updated_dead_uuid_list = ','.join(set(dead_uuid_list))
-                vmScaleSetTags['dead_uuid_list'] = updated_dead_uuid_list
+        if vmss_data['disaster']:
+            correlationId = vmss_data['data']['context']['activityLog']['correlationId']
+            if uuid in vmScaleSetTags:
+                if vmScaleSetTags[uuid] == correlationId:
+                    logging.info('No need to update dead_uuid_list Tag,As this is a Re-Run for correlationId:{}'.format(correlationId))
+                    return True
+                else:
+                    logging.info('Set Existing uuid to CorrelationID mapping ==> {}:{}'.format(uuid,correlationId))
+                    vmScaleSetTags[uuid] = correlationId
             else:
-                vmScaleSetTags['dead_uuid_list'] = uuid
-            vmScaleSetTags['dead_uuid_buffer'] = uuid
+                vmScaleSetTags[uuid] = correlationId
+                logging.info('Set uuid to CorrelationID mapping for First Time==> {}:{}'.format(uuid,correlationId))
+        #else:
+        if len(dead_uuid_list) > 0:
+            dead_uuid_list = dead_uuid_list.split(',')
+            dead_uuid_list.append(uuid)
+            updated_dead_uuid_list = ','.join(set(dead_uuid_list))
+            vmScaleSetTags['dead_uuid_list'] = updated_dead_uuid_list
+        else:
+            vmScaleSetTags['dead_uuid_list'] = uuid
     except KeyError:
         vmScaleSetTags['dead_uuid_list'] = uuid
-        vmScaleSetTags['dead_uuid_buffer'] = uuid
     logging.info('Updating Scale Set : {} with tag dead_uuid_list with value {}'.format(vmScaleSetName,vmScaleSetTags['dead_uuid_list']))
     create_vmss_tag(compute,vmScaleSetName,resourceGroupName,location,vmScaleSetTags)
 
@@ -53,9 +60,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info ("Scale Operation : {}".format(req.get_json()))
     logging.info("Component type received {}".format(req.params.get("compType")))
     scale_operation = req.get_json()
+    scale_operation['disaster'] = False
     if 'schemaId' in scale_operation:
         if 'Microsoft.Insights/activityLogs' in scale_operation['schemaId']:
             scale_operation['operation'] = 'Scale In'
+            scale_operation['disaster'] = True
             scale_operation['context'] = {
                 'subscriptionId': scale_operation['data']['context']['activityLog']['subscriptionId'],
                 'resourceGroupName': scale_operation['data']['context']['activityLog']['resourceGroupName'],
